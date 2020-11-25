075f5ca27ed819aa8f004e011aab8d3a3f728870
yulingsong <yulingsong5@126.com>
2020-11-25 11:12:59 +0800

    0.0.1
    打包上传

--- trunk/config/dev.env.js
+++ trunk/config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
     // BASE_URL: '"http://182.92.173.111:3589/api/v1/"', // 新增 DEV
-    BASE_URL: '"https://www.61huoban.com"', // 新增 DEV
+    BASE_URL: '"https://www.61huoban.com/api/v1/"', // 新增 DEV
 })
