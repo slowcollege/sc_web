b2da74e5dbc07ae29e4a01fcc3f3b90e011b0f4c
yulingsong <yulingsong5@126.com>
2020-11-25 11:11:17 +0800

    0.0.1
    打包

--- trunk/config/prod.env.js
+++ trunk/config/prod.env.js
@@ -1,5 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-    BASE_URL: '"http://182.92.173.111:3589/api/v1/"', // 新增 DEV
+    // BASE_URL: '"http://182.92.173.111:3589/api/v1/"', // 新增 DEV
+    BASE_URL: '"https://www.61huoban.com"', // 新增 DEV
+
 }
